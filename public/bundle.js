d5b41708b9b6eaeba71e52dc23ecaabde3525816
David-F-98 <u2903149@unimilitar.edu.co>
2023-12-27 14:32:23 -0500

    Btn Agregar Carrito

--- public/bundle.js
+++ public/bundle.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const producto = document.getElementById('producto');
-const productoImagen = producto.querySelector('.producto__imagen');
-const thumbs = producto.querySelector('.producto__thumbs');
+const producto$1 = document.getElementById('producto');
+const productoImagen = producto$1.querySelector('.producto__imagen');
+const thumbs = producto$1.querySelector('.producto__thumbs');
 
-const propiedadColor = producto.querySelector('#propiedad-color');
+const propiedadColor = producto$1.querySelector('#propiedad-color');
 
-const botonIncrementarCant = producto.querySelector('#incrementar-cantidad');
-const botonDisminuirCant = producto.querySelector('#disminuir-cantidad');
-const inputCantidad = producto.querySelector('#cantidad');
+const botonIncrementarCant = producto$1.querySelector('#incrementar-cantidad');
+const botonDisminuirCant = producto$1.querySelector('#disminuir-cantidad');
+const inputCantidad = producto$1.querySelector('#cantidad');
 
 
 thumbs.addEventListener('click', (e)=>{
@@ -47,9 +47,14 @@ botonIncrementarCant.addEventListener('click', (e)=>{
 const btnAbrirCarrito = document.querySelectorAll('[data-accion="abrir-carrito"]');
 const btnCerrarCarrito = document.querySelectorAll('[data-accion="cerrar-carrito"]');
 const ventanaCarrito = document.getElementById('carrito');
+const btnAgregarCarrito = document.getElementById('agregar-al-carrito');
+const producto = document.getElementById('producto');
+const carrito = [];
+
 
 const renderCarrito = () =>{
     ventanaCarrito.classList.add('carrito--active');
+    console.log(carrito);
 };
 
 btnAbrirCarrito.forEach((botones)=>{
@@ -61,5 +66,20 @@ btnAbrirCarrito.forEach((botones)=>{
 btnCerrarCarrito.forEach((botones)=>{
     botones.addEventListener('click',()=>{
         ventanaCarrito.classList.remove('carrito--active');
+    });
+});
+
+btnAgregarCarrito.addEventListener('click', (e)=>{
+    const id = producto.dataset.productoId;
+    const nombre = producto.querySelector('.producto__nombre').innerText;
+    const cantidad = parseInt(producto.querySelector('#cantidad').value);
+    const color = document.querySelector('#propiedad-color input:checked').value;
+    const tamano = document.querySelector('#propiedad-tamaño input:checked').value;
+    carrito.push({
+        id : id,
+        nombre:nombre,
+        cantidad:cantidad,
+        color:color,
+        tamano:tamano,
     });
 });
--- src/carrito.js
+++ src/carrito.js
@@ -1,9 +1,14 @@
 const btnAbrirCarrito = document.querySelectorAll('[data-accion="abrir-carrito"]');
 const btnCerrarCarrito = document.querySelectorAll('[data-accion="cerrar-carrito"]');
 const ventanaCarrito = document.getElementById('carrito');
+const btnAgregarCarrito = document.getElementById('agregar-al-carrito');
+const producto = document.getElementById('producto');
+const carrito = [];
+
 
 const renderCarrito = () =>{
     ventanaCarrito.classList.add('carrito--active');
+    console.log(carrito);
 };
 
 btnAbrirCarrito.forEach((botones)=>{
@@ -17,3 +22,18 @@ btnCerrarCarrito.forEach((botones)=>{
         ventanaCarrito.classList.remove('carrito--active');
     });
 });
+
+btnAgregarCarrito.addEventListener('click', (e)=>{
+    const id = producto.dataset.productoId;
+    const nombre = producto.querySelector('.producto__nombre').innerText;
+    const cantidad = parseInt(producto.querySelector('#cantidad').value);
+    const color = document.querySelector('#propiedad-color input:checked').value;
+    const tamano = document.querySelector('#propiedad-tamaño input:checked').value;
+    carrito.push({
+        id : id,
+        nombre:nombre,
+        cantidad:cantidad,
+        color:color,
+        tamano:tamano,
+    });
+})
