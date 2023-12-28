535520ba574a08201f110ef5d57b416e55f481d3
David-F-98 <u2903149@unimilitar.edu.co>
2023-12-27 19:07:15 -0500

    Carga de Productos

--- src/carrito.js
+++ src/carrito.js
@@ -5,10 +5,56 @@ const btnAgregarCarrito = document.getElementById('agregar-al-carrito');
 const producto = document.getElementById('producto');
 const carrito = [];
 
-
 const renderCarrito = () =>{
     ventanaCarrito.classList.add('carrito--active');
-    console.log(carrito);
+
+    const productosAnteriores = ventanaCarrito.querySelectorAll('.carrito__producto');
+    productosAnteriores.forEach((producto)=>producto.remove());
+
+    carrito.forEach((productoCarrito)=>{
+        let thumbSrc = producto.querySelectorAll('.producto__thumb-img')[0].src;
+        if(productoCarrito.color === 'rojo'){
+            thumbSrc = './img/thumbs/rojo.jpg';
+        } else if(productoCarrito.color === 'amarillo'){
+            thumbSrc = './img/thumbs/amarillo.jpg';
+        }    
+
+        const plantillaProducto = `
+        <div class="carrito__producto-info">
+            <img src="${thumbSrc}" alt="" class="carrito__thumb" />
+                <div>
+                    <p class="carrito__producto-nombre">
+                        <span class="carrito__producto-cantidad">${productoCarrito.cantidad} x </span>${productoCarrito.nombre}
+                    </p>
+                    <p class="carrito__producto-propiedades">
+                        Tamaño:<span>${productoCarrito.tamano}</span> Color:<span>${productoCarrito.color}</span>
+                    </p>
+                </div>
+        </div>
+        <div class="carrito__producto-contenedor-precio">
+            <button class="carrito__btn-eliminar-item" data-accion="eliminar-item-carrito">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                >
+                    <path
+                        d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"
+                    />
+                </svg>
+            </button>
+            <p class="carrito__producto-precio">$500.00</p>
+        </div>
+        `;
+
+        const itemCarrito = document.createElement('div');
+
+        itemCarrito.classList.add('carrito__producto');
+        itemCarrito.innerHTML = plantillaProducto;
+        ventanaCarrito.querySelector('.carrito__body').appendChild(itemCarrito);
+    })
 };
 
 btnAbrirCarrito.forEach((botones)=>{
@@ -27,8 +73,8 @@ btnAgregarCarrito.addEventListener('click', (e)=>{
     const id = producto.dataset.productoId;
     const nombre = producto.querySelector('.producto__nombre').innerText;
     const cantidad = parseInt(producto.querySelector('#cantidad').value);
-    const color = document.querySelector('#propiedad-color input:checked').value;
-    const tamano = document.querySelector('#propiedad-tamaño input:checked').value;
+    const color = producto.querySelector('#propiedad-color input:checked').value;
+    const tamano = producto.querySelector('#propiedad-tamaño input:checked').value;
     carrito.push({
         id : id,
         nombre:nombre,
